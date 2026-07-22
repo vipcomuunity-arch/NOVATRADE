d73d312956ebbc66b8774cbd2aa38e16159b47d5
vipcomuunity-arch <vipcomuunity@gmail.com>
2026-07-22 23:50:46 +0300

    Fix formatting of import statements in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/global.css'
-import './styles/terminal.css'
-import './styles/auth.css'
-import './styles/pages.css'
-import './styles/chart.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/global.css';
+import './styles/terminal.css';
+import './styles/auth.css';
+import './styles/pages.css';
+import './styles/chart.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
